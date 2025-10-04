d5cdc13d959dd752af6656f03d75fdd2853f4f20
GylkaRoman <gylkaroman1.gmail.com>
2025-10-04 21:29:18 +0300

    Добавил аннотации для всех классов в model как аннотации: @Entity @Data @NoArgsConstructor @AllArgsConstructor @Builder

--- src/main/java/org/example/taxiservice/model/Location.java
+++ src/main/java/org/example/taxiservice/model/Location.java
@@ -1,4 +1,15 @@
 package org.example.taxiservice.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Location {
 }
