565bd011aaf1a8527b12eb09d5eee34943674f3d
GylkaRoman <gylkaroman1.gmail.com>
2025-10-06 15:29:29 +0300

    V1.1 Реализовано всё необходимое в основных директориях как service и impl, controller, entity в model и repository перед тестами через PostMan и подключение к PostgreSQL хочу сразу сделать DTO и Validation.

--- src/main/java/org/example/taxiservice/TaxiServiceApplication.java
+++ src/main/java/org/example/taxiservice/TaxiServiceApplication.java
@@ -2,6 +2,6 @@ package org.example.taxiservice;
 
 public class TaxiServiceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("V1.1");
     }
 }
